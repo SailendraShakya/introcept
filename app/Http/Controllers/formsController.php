0b4cd3ad25c40231361dcff51c0ea4e301454b8c
Sailendra Shakya <sailendrashakya@Sailendras-MacBook-Pro.local>
2017-09-01 14:00:37 +0545

    added form controller

---
+++ app/Http/Controllers/formsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FormsController extends Controller {
+
+	public function getIndex(){
+		return view('pages/welcome');
+	}
+
+	public function getAbout(){
+		$first = 'Alex';
+		$last = 'Shakya';
+		$full = $first . ' ' . $last;
+		return view('pages.about')->with('fullname',$full);
+	}
+
+	public function getContact(){
+		return view('pages/contact');
+	}
+} 
