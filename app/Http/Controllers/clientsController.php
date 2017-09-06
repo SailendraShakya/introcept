a00828ee46b8d6b9e3ce247855786ded18738f0d
Sailendra Shakya <sailendrashakya@Sailendras-MacBook-Pro.local>
2017-09-06 12:07:24 +0545

    added some log message in client controller

--- app/Http/Controllers/clientsController.php
+++ app/Http/Controllers/clientsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ClientsController extends Controller {
 
@@ -12,6 +13,7 @@ class ClientsController extends Controller {
      * @return Response
      */
 	public function getIndex(){
+        Log::info('Showing add client form for user.');
 		return view('form');
 	}
 
@@ -21,6 +23,7 @@ class ClientsController extends Controller {
      * @return Response
      */
 	public function listing(){
+        Log::info('Listing client for user');
 		$file = file_get_contents("csv/data.csv");
 		$data = array_map("str_getcsv", preg_split('/\r*\n+|\r+/', $file));
 		return view('listing')->with('records', array_reverse($data));
@@ -34,6 +37,7 @@ class ClientsController extends Controller {
      */
     public function store(Request $request)
     {
+        Log::info('Validation rule for client');
         $this->validate($request, [
         'name' => 'required|max:255',
         'gender' => 'required|max:255',
@@ -55,14 +59,15 @@ class ClientsController extends Controller {
     	$date_of_birth = $request->input('date_of_birth');
     	$education = $request->input('education');
     	$contact = $request->input('contact');
-
+        Log::info('Getting value from form.');
 		$cvsData = array($name,$gender,$phone,$email,$address,$nationality,$date_of_birth,$education,$contact);
 		$file_name = public_path('csv/data.csv');
 		$fp = fopen($file_name,"a");
 		if($fp)
-		{
-		fputcsv($fp, $cvsData);
-		fclose($fp);
+		{   
+            Log::info('Adding client to csv file');
+            fputcsv($fp, $cvsData);
+            fclose($fp);
 		}
         return redirect('client/listing');
 
